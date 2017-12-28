dbdfba654a7a28efcb419fd32ac9e5e86a708071
sammieliu <sammieliu@tencent.com>
2017-12-28 20:55:52 +0800

    aa

--- ssl/gencert.sh
+++ ssl/gencert.sh
@@ -19,25 +19,30 @@ function color_red()
 function echo_green()
 {
   color_green
-  echo "$*"
+  echo -e "$*"
   color_off
 }
 function echo_red()
 {
   color_red
-  echo "$*"
+  echo -e "$*"
   color_off
+}
+
+function input_domain()
+{
+  read -p "Enter you domain [$DEFAULT_DOMAIN]: " DOMAIN
+  if [ "x$DOMAIN" == "x" ]; then
+    DOMAIN=$DEFAULT_DOMAIN
+  fi
+  echo_green "domain: $DOMAIN"
 }
 
 function create_root_ca()
 {
   local subject
 
-  read -p "Enter you domain [$DEFAULT_DOMAIN]: " DOMAIN
-  if [ "x$DOMAIN" == "x" ]; then
-    DOMAIN=$DEFAULT_DOMAIN
-  fi
-  echo_green "domain: $DOMAIN"
+  input_domain
 
   subject="/C=CN/CN=$DOMAIN self-signed CA"
   echo_green "generate root CA key: $ROOTCA_KEY"
@@ -58,6 +63,14 @@ function create_site_ca()
   local site_crt=$DOMAIN.crt
   local extension=extension.conf
 
+  if ! [ -r $ROOTCA_CRT ] || ! [ -r $ROOTCA_KEY ]; then
+    echo_red "Cannot read both $ROOTCA_CRT and $ROOTCA_KEY"
+    echo_red "Please use --new_root or -n option"
+    return 1
+  fi
+
+  input_domain
+
   subject="/CN=$DOMAIN/O=${DOMAIN}/C=CN"
 
   echo_green "Create private key: $site_key"
@@ -76,5 +89,45 @@ function create_site_ca()
     -extfile $extension -CAcreateserial
 }
 
-create_root_ca
+function clear_all()
+{
+  rm -fv *.key *.csr *.crt *.conf *srl
+}
+
+function usage()
+{
+  echo_green "usage: $0 -h|--help -n|--new_root -c|--clear"
+  echo_green "\t: -h|--help, show this help message"
+  echo_green "\t: -n|--new_root, new root CA"
+  echo_green "\t: -c|--clear, clear all temp file"
+}
+
+TEMP=`getopt -o h,n,c -l help,new_root,clear -- "$@"`
+if [ $? -ne 0 ]; then
+  echo_red "wrong option"
+  usage
+  exit 1
+fi
+eval set -- $TEMP
+
+while [ $# -gt 0 ]; do
+  case "$1" in
+    -h|--help)
+      usage
+      exit 0
+      ;;
+    -n|--new_root)
+      create_root_ca
+      ;;
+    -c|--clear)
+      clear_all
+      exit 0
+      ;;
+    --)
+      ;;
+    *)
+      ;;
+  esac
+  shift
+done
 create_site_ca
